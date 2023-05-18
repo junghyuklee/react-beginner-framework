2f2be33c525e4355b249ad3da744aa9aaf075496
coDev <jh900127@gmail.com>
2023-05-18 23:06:32 +0900

    react beginner init

---
+++ src/components/MovieInfo.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function MovieInfo({ id, coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+MovieInfo.propTypes = {
+  id: PropTypes.string.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string.isRequired),
+};
+export default MovieInfo;
---
+++ src/routes/Detail.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movieInfo, setMovieInfo] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json? movie_id=${id}`)
+    ).json();
+    setMovieInfo(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movieInfo.large_cover_image} alt={movieInfo.title} />
+          <h3>
+            {movieInfo.title} ({movieInfo.year})
+          </h3>
+          <p>{movieInfo.description_full}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Detail;
